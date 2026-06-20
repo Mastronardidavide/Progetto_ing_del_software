533b2335b90013dfdba6d5cbd15140fde306b75f
FedericoBattistiFAF <fede.fede.battisti05@gmail.com>
2026-06-20 18:11:54 +0200

    codice commentato

--- domOS/Repos/dispositivo_repository.py
+++ domOS/Repos/dispositivo_repository.py
@@ -10,10 +10,12 @@ class DispositivoRepository:
         #dizionario che gli pass come chiave l'ID e come valore l'ogetto sensore o attuatore
         self.carica()
 
-    def _risolvi_path(self, path: str) -> Path:
+    #risolvi path mi permette di controllare se il path è assoluto, in caso non lo sia, ci spostiamo sopra di due
+    #passi nel path, e creiamo il percorso direttamente a partire dalla radice
+    def _risolvi_path(self, path: str) -> Path: #necessaria l'implementazione per lavorare sugli stessi file
         percorso = Path(path)
-        if not percorso.is_absolute():
-            percorso = Path(__file__).resolve().parent.parent / percorso
+        if not percorso.is_absolute(): #controlliamo se il percorso è assoluto così da non avere conflitti
+            percorso = Path(__file__).resolve().parent.parent / percorso #se non è assoluto lo "costruiamo" noi
         return percorso
 
     def carica(self) -> None:
@@ -34,9 +36,10 @@ class DispositivoRepository:
         except (FileNotFoundError, json.JSONDecodeError):
             self._dispositivi = {}
     
+    #implementiamo salva in un modo che ci permette di evitare conflitti di path
     def salva(self) -> None:
-        self._path.parent.mkdir(parents=True, exist_ok=True)
-        with open(self._path, "w", encoding="utf-8") as f:
+        self._path.parent.mkdir(parents=True, exist_ok=True) #controllo che Data esista effettivamente dove viene 
+        with open(self._path, "w", encoding="utf-8") as f: #eseguito il codice, altrimenti la creo
             json.dump([d.toDict() for d in self._dispositivi.values()],f, indent=2, ensure_ascii=False)
 
     def trovaPerId(self, id_dispositivo: str):
